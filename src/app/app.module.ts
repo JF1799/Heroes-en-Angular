fa40d2a0d5d8877ab934ece968a63b067d978a7f
Jeff Molina <jeffersogmb7@gmail.com>
2020-11-17 15:56:14 -0600

    1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
+import { APP_ROUTING } from './app.routes';
+
+//Servicios
+import { HeroesService } from './servicios/heroes.service';
+
+//Componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/shared/navbar/navbar.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { HeroesComponent } from './componentes/heroes/heroes.component';
+import { HeroeComponent } from './componentes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, APP_ROUTING],
+  providers: [HeroesService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
